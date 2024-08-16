145bea59dd95c6e3176e8139e2dfe6c46ac7ce3c
Minikon <112776259+Minikon3@users.noreply.github.com>
2024-08-16 21:43:12 +0300

    Add files via upload

--- versionbuttons.js
+++ versionbuttons.js
@@ -6,7 +6,13 @@ function switchVersion(version) {
 
     // Показываем выбранный контейнер версии
     const selectedContainer = document.querySelector(`.version-container[data-version="${version}"]`);
-    selectedContainer.style.display = 'flex';
+    // Определяем нужное значение display в зависимости от ширины экрана
+    if (window.innerWidth <= 768) {
+        selectedContainer.style.display = 'block';  // Мобильные устройства
+        selectedContainer.style.width = '90%'
+    } else {
+        selectedContainer.style.display = 'flex';   // Десктопы
+    }
 }
 
 // Привязка события к кнопкам
@@ -22,6 +28,11 @@ document.querySelectorAll('.version-button').forEach(button => {
 
 // Восстанавливаем версию при загрузке
 document.addEventListener('DOMContentLoaded', () => {
+    const savedVersion = localStorage.getItem('selectedVersion') || '1.0';
+    switchVersion(savedVersion);
+});
+// Обработчик для изменения ориентации устройства или изменения размера окна
+window.addEventListener('resize', () => {
     const savedVersion = localStorage.getItem('selectedVersion') || '1.0';
     switchVersion(savedVersion);
 });
