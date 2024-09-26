5a3b2f9ae8d60d9f7b01f2b9ee3bd55a75f2ea54
Minikon <112776259+Minikon3@users.noreply.github.com>
2024-09-26 21:31:31 +0300

    Add files via upload

---
+++ versionbuttons_forcoins.js
@@ -0,0 +1,38 @@
+function switchVersion(version) {
+    // Скрываем все контейнеры версий
+    document.querySelectorAll('.version-container').forEach(container => {
+        container.style.display = 'none';
+    });
+
+    // Показываем выбранный контейнер версии
+    const selectedContainer = document.querySelector(`.version-container[data-version="${version}"]`);
+    // Определяем нужное значение display в зависимости от ширины экрана
+    if (window.innerWidth <= 768) {
+        selectedContainer.style.display = 'block';  // Мобильные устройства
+        selectedContainer.style.width = '100%'
+    } else {
+        selectedContainer.style.display = 'flex';   // Десктопы
+    }
+}
+
+// Привязка события к кнопкам
+document.querySelectorAll('.version-button').forEach(button => {
+    button.addEventListener('click', () => {
+        const version = button.getAttribute('data-version');
+        switchVersion(version);
+
+        // Сохраняем выбранную версию в localStorage
+        localStorage.setItem('selectedVersion_forcoins', version);
+    });
+});
+
+// Восстанавливаем версию при загрузке
+document.addEventListener('DOMContentLoaded', () => {
+    const savedVersion = localStorage.getItem('selectedVersion_forcoins') || '1.0';
+    switchVersion(savedVersion);
+});
+// Обработчик для изменения ориентации устройства или изменения размера окна
+window.addEventListener('resize', () => {
+    const savedVersion = localStorage.getItem('selectedVersion_forcoins') || '1.0';
+    switchVersion(savedVersion);
+});
